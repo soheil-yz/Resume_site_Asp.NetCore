8baa4f95f6cdeba964eeb5ffbc4234b69b739777
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-09-29 13:02:51 +0330

    .

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MYProfilo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
